86c540ae776db9c32f4ab59b5a71030864f96e90
J-HON404 <mattia.developer12@gmaill,com>
2025-12-11 21:50:11 +0100

    Refactoring completo del codice

--- src/main/java/com/unicam/cs/progettoweb/marketplace/notification/NotificationMessage.java
+++
@@ -1,7 +0,0 @@
-package com.unicam.cs.progettoweb.marketplace.notification;
-
-public interface NotificationMessage {
-    Long getRecipientId();
-    String getContent();
-    String getType();
-}
--- src/main/java/com/unicam/cs/progettoweb/marketplace/notification/channel/NotificationChannel.java
+++
@@ -1,7 +0,0 @@
-package com.unicam.cs.progettoweb.marketplace.notification.channel;
-
-import com.unicam.cs.progettoweb.marketplace.notification.NotificationMessage;
-
-public interface NotificationChannel {
-    void sendNotification(NotificationMessage notice);
-}
