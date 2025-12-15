403a5cf462a8be3690ee95dfad94b09e7c68239d
J-HON404 <mattia.developer12@gmaill,com>
2025-12-15 10:20:31 +0100

    cambiamenti strutturali

--- marketplace-backend/src/main/java/com/unicam/cs/progettoweb/marketplace/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.unicam.cs.progettoweb.marketplace.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
--- marketplace-backend/src/main/java/com/unicam/cs/progettoweb/marketplace/model/enums/TypeProductNotice.java
+++
@@ -1,4 +0,0 @@
-package com.unicam.cs.progettoweb.marketplace.model.enums;
-public enum TypeProductNotice {
-    AVAILABILITY,STOCK,PROMOTION,WARNING,INFO
-}
