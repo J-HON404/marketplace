3ba06e1962abb63f854620ecd18bb349a8401b72
J-HON404 <148477895+J-HON404@users.noreply.github.com>
2026-03-06 15:18:05 +0100

    Delete marketplace-backend directory

--- marketplace-backend/src/main/java/com/unicam/cs/progettoweb/marketplace/model/enums/TypeProductNotice.java
+++
@@ -1,9 +0,0 @@
-package com.unicam.cs.progettoweb.marketplace.model.enums;
-
-/**
- * Enum che rappresenta i tipi di ProductNotice (avvisi) associate a un prodotto.
- */
-
-public enum TypeProductNotice {
-    AVAILABILITY,STOCK,PROMOTION,WARNING,INFO
-}
