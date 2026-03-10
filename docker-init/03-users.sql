158a00598f617cc39d85959a944836679b26dbdf
J-HON404 <mattia.developer12@gmaill,com>
2026-03-10 17:09:19 +0100

    Aggiornato file per la creazione profili mysql per il db

--- docker-init/03-users.sql
+++ docker-init/03-users.sql
@@ -1,10 +1,10 @@
 USE marketplace;
 
--- backend user
+-- backend user profile
 CREATE USER IF NOT EXISTS 'marketuser'@'%' IDENTIFIED BY 'xxxxxxx';
 GRANT ALL PRIVILEGES ON marketplace.* TO 'marketuser'@'%';
 
--- auth user
+-- auth user profile
 CREATE USER IF NOT EXISTS 'authuser'@'%' IDENTIFIED BY 'xxxxxxxx';
 GRANT SELECT, INSERT, UPDATE ON marketplace.profiles TO 'authuser'@'%';
 GRANT SELECT, INSERT, UPDATE ON marketplace.shops TO 'authuser'@'%';
