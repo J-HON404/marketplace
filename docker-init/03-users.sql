32cc293b79593902a44a93242e2fb7168eb895d7
J-HON404 <148477895+J-HON404@users.noreply.github.com>
2026-03-10 17:39:08 +0100

    Update 03-users.sql

--- docker-init/03-users.sql
+++ docker-init/03-users.sql
@@ -1,11 +1,11 @@
 USE marketplace;
 
 -- backend user profile
-CREATE USER IF NOT EXISTS 'marketuser'@'%' IDENTIFIED BY 'xxxxxxx';
+CREATE USER IF NOT EXISTS 'marketuser'@'%' IDENTIFIED BY 'marketpass';
 GRANT ALL PRIVILEGES ON marketplace.* TO 'marketuser'@'%';
 
 -- auth user profile
-CREATE USER IF NOT EXISTS 'authuser'@'%' IDENTIFIED BY 'xxxxxxxx';
+CREATE USER IF NOT EXISTS 'authuser'@'%' IDENTIFIED BY 'authpass';
 GRANT SELECT, INSERT, UPDATE ON marketplace.profiles TO 'authuser'@'%';
 GRANT SELECT, INSERT, UPDATE ON marketplace.shops TO 'authuser'@'%';
 
