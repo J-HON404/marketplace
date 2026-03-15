9808cac2c429c667dfa7a992de33f1b32ebb4bfa
J-HON404 <148477895+J-HON404@users.noreply.github.com>
2026-03-15 19:25:54 +0100

    Delete marketplace-backend directory

--- marketplace-backend/src/main/java/com/unicam/cs/progettoweb/marketplace/model/enums/ProfileRole.java
+++
@@ -1,9 +0,0 @@
-package com.unicam.cs.progettoweb.marketplace.model.enums;
-
-/**
- * Enum che rappresenta i ruoli di un profilo nel marketplace.
- */
-
-public enum ProfileRole {
-    CUSTOMER, SELLER
-}
